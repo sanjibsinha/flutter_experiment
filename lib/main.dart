6f161b62e68dcc1e5a191183d53f7f33d353178a
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-08-08 08:46:29 +0530

    First

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Container example"),
-        ),
-        body: Container(
-          height: 200,
-          width: double.infinity,
-          color: Colors.red.shade200,
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(30),
-          transform: Matrix4.rotationZ(0.10),
-          child: Text(
-            'Child Text Inside Container',
-            style: GoogleFonts.alike(
-              textStyle: Theme.of(context).textTheme.displayMedium,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Scaffold Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+              ),
+              child: Text(
+                'Scaffold Example',
+                style: TextStyle(
+                  color: Colors.red.shade500,
+                  fontSize: 24,
+                ),
+              ),
             ),
-          ),
+            const ListTile(
+              title: Text('Page One'),
+            ),
+            const ListTile(
+              title: Text('Page Two'),
+            ),
+          ],
         ),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style: GoogleFonts.cantataOne(
+                fontSize: 30.00,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          fixedColor: Colors.redAccent,
+          items: const [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Search",
+              icon: Icon(Icons.search),
+            ),
+          ],
+          onTap: (int
+              indexOfItem) {}), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
