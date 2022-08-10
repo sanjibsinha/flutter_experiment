76aea677820c62605d5e23de7720f07e34c443f3
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-08-10 05:44:21 +0530

    First

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// function to trigger app build
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  final appTitle = 'Flutter Drawer Example';
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App Example',
-      //theme: ThemeData(primarySwatch: Colors.indigo),
-      /// nothing happens
-      ///
-      darkTheme: ThemeData(primarySwatch: Colors.blueGrey),
-      color: Colors.amberAccent,
-      supportedLocales: {const Locale('en', ' ')},
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Example'),
-        ),
-        body: Center(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    ); // MaterialApp
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title), backgroundColor: Colors.redAccent),
+      body: Center(
           child: Container(
-            width: double.infinity,
-            margin: const EdgeInsets.all(30.0),
-            child: Text(
-              'Material App Example',
-              style: GoogleFonts.mavenPro(
-                fontSize: 60.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+        margin: const EdgeInsets.all(20.0),
+        child: Text(
+          'Home Page. We cannot see a drawer unless we click.',
+          style: GoogleFonts.salsa(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
-      ),
+      )),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.redAccent,
+              ), //BoxDecoration
+              child: UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(color: Colors.redAccent),
+                accountName: const Text(
+                  'John Smith',
+                  style: TextStyle(fontSize: 18),
+                ),
+                accountEmail: const Text("js123@gmail.com"),
+                currentAccountPictureSize: const Size.square(50),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  child: Text(
+                    "JS",
+                    style: GoogleFonts.aldrich(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text(' Home '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text(' About '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.workspace_premium),
+              title: const Text(' Contact '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.video_label),
+              title: const Text(' Privacy Policy '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text(' Terms and Conditions '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('LogOut'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ), //Deawer
     );
   }
 }
